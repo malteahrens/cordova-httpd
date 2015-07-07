c96ecb3cd46b39ad7470f16a9e64ade6fb71af45
Malte Ahrnes <MalteAhrens@arcor.de>
2015-07-07 19:45:29 +0200

    test

--- src/android/CorHttpd.java
+++ src/android/CorHttpd.java
@@ -147,7 +147,8 @@ public class CorHttpd extends CordovaPlugin {
     		f.setAssetManager( am );
     		
     		if(localhost_only) {
-    			InetSocketAddress localAddr = InetSocketAddress("127.0.0.1", port);
+                String localHost = __getLocalIpAddress()
+    			InetSocketAddress localAddr = InetSocketAddress.createUnresolved(localHost, port);
     			server = new WebServer(localAddr, f);
     		} else {
     			server = new WebServer(port, f);
